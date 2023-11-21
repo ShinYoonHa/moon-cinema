d886c500ce8dd66e948a93aefd5e6b2a963916f2
SinYoonHa <idc06012@naver.com>
2023-11-21 15:30:57 +0900

    middle 9 commits

--- src/app-config.js
+++ src/app-config.js
@@ -3,7 +3,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-  backendHost = "http://172.30.74.17:8080";
+  backendHost = "https://172.30.74.17:8080";
 }
 
 export const API_BASE_URL = "https://172.30.74.17:8080";
