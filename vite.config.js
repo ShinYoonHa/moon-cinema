39e8809a64355aa5170e552101315f016b36a621
SinYoonHa <idc06012@naver.com>
2023-11-21 10:27:31 +0900

    mend

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,6 @@ import eslint from "vite-plugin-eslint";
 export default defineConfig({
   plugins: [react(), eslint()],
   build: {
-<<<<<<< HEAD:vite.config.js
     outDir: "build",
-=======
-    outDir: 'build',
->>>>>>> 9fb453dba87c7b22469efed38249f26a8d0cf652:vite.config.ts
   },
 });
